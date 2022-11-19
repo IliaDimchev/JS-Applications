1589372e5e9d3c9325ffbde5166fc76e992ef3b1
ilkoTheTiger <ilkothetiger@gmail.com>
2022-11-19 08:08:42 +0200

    phonebook solution added

--- 02.Phonebook/app.js
+++ 02.Phonebook/app.js
@@ -1,84 +1,66 @@
 function attachEvents() {
-    document.getElementById("btnLoad").addEventListener("click", onLoadAllRecords);
-    document.getElementById("btnCreate").addEventListener("click", handleCreateRecord);
-}
+    const url = "http://localhost:3030/jsonstore/phonebook/"
+    const btnLoad = document.getElementById('btnLoad');
+    btnLoad.addEventListener('click', onLoad);
 
-function handleCreateRecord() {
-    const personEl = document.getElementById("person");
-    const phoneEl = document.getElementById("phone");
+    const btnCreate = document.getElementById('btnCreate');
+    btnCreate.addEventListener('click', onCreate);
 
-    onCreateRecord(personEl.value, phoneEl.value);
-    personEl.value = "";
-    phoneEl.value = "";
-}
-
-function renderRecord(data) {
-    const ul = document.getElementById("phonebook");
-    ul.innerHTML = "";
-    Object.values(data).forEach(rec => {
-        const li = document.createElement("li");
-        li.textContent = `${rec.person}: ${rec.phone}`
-        li.setAttribute("data-id", rec._id);
-        
-        const btn = document.createElement("button");
-        btn.textContent = "Delete";
-        btn.addEventListener("click", handleDelete)
-        li.appendChild(btn);
-        ul.appendChild(li);
-
-  
-    })
-}
-
-function handleDelete(e) {
-    const li = e.target.parentElement;
-    const id = li.getAttribute("data-id");
+    const phones = document.getElementById('phonebook');
+    const person = document.getElementById('person');
+    const phone = document.getElementById('phone');
     
-    onDeleteRecord(id);
-    li.remove();
-}
 
-async function onLoadAllRecords() {
-    const url = "http://localhost:3030/jsonstore/phonebook"
-    const response = await fetch(url);
-    const data = await response.json();
+    async function onLoad(e) {
+        phones.innerHTML = "";
+        const response = await fetch(url);
+        const data = await response.json();
+        for (contact in data){
+            const {person, phone, _id} = data[contact]
+            const li = document.createElement('li')
+            li.textContent = `${person}: ${phone}`;
+            li.setAttribute('data-id', _id)
 
-    return renderRecord(data);
-}
+            const btn = document.createElement("button");
+            btn.textContent = "Delete"
+            btn.addEventListener('click', onDelete);
+            li.appendChild(btn)
+            phones.appendChild(li)
 
-
-async function onCreateRecord(person, phone) {
-    const url = "http://localhost:3030/jsonstore/phonebook"
-    const body = {
-        person,
-        phone
+        }
+        return data;
     }
-    const header = getHeader("POST", body);
-    const response = await fetch(url, header);
 
-    const data = await response.json();
-    onLoadAllRecords();
-    return data;
-
-}
-
-async function onDeleteRecord(id) {
-    const url = `http://localhost:3030/jsonstore/phonebook/${id}`
-
-    const header = getHeader("DELETE", null)
-    const response = await fetch(url, header)
-
-    const data = await response.json();
-    return data;
-}
-
-function getHeader(method, body) {
-    return {
-        method: `${method}`,
-        headers: {
+    async function onDelete(e) {
+        const key = e.target.parentElement.getAttribute('data-id')
+        const response = await fetch(url + key, {
+            method: "DELETE",
             "Content-Type": "application/json"
-        },
-        body: JSON.stringify(body)
+        })
+        const data = await response.json();
+        onLoad();
+        return data;
     }
-}
+
+    async function onCreate(e) {
+        if (person.value == "" || phone.value == ""){
+            return
+        }
+        const response = await fetch(url, {
+            method: "POST",
+            "Content-Type": "application/json",
+            body: JSON.stringify({
+                "person": `${person.value}`,
+                "phone": `${phone.value}`
+            })
+        });
+        const data = await response.json();
+        phone.value = ""
+        person.value = ""
+        onLoad();
+        return data;
+    }
+
+};
+
 attachEvents();
