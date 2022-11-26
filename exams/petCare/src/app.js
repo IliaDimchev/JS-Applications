012cda3d14fac1da4123adeccbc9f6db3ba9f252
ilkoTheTiger <ilkothetiger@gmail.com>
2022-11-26 14:22:16 +0200

    homework and exam

---
+++ exams/petCare/src/app.js
@@ -0,0 +1,42 @@
+import { page, render } from "./lib.js";
+import { showCatalog } from "./views/catalog.js";
+import { showHome } from "./views/home.js";
+import { showLogin } from "./views/login.js";
+import { showRegister } from "./views/register.js";
+import { updateNav } from "./views/nav.js";
+import { getUserData } from "./util.js";
+import { showDetails } from "./views/details.js";
+import { showCreate } from "./views/create.js";
+import { showEdit } from "./views/edit.js";
+
+const main = document.getElementById('content');
+
+page(decorateContext);
+page('/', showHome);
+page('/catalog', showCatalog);
+page('/catalog/:id', showDetails);
+page('/edit/:id', showEdit);
+page('/create', showCreate);
+page('/login', showLogin);
+page('/register', showRegister);
+
+updateNav();
+page.start();
+
+function decorateContext(ctx, next) {
+    ctx.render = renderMain;
+    ctx.updateNav = updateNav;
+
+    const user = getUserData();
+    if (user){
+        ctx.user = user;
+    }
+
+    next();
+}
+
+function renderMain(content) {
+    render(content, main);
+}
+
+
